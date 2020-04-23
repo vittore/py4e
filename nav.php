80995788cee262c49aafe9f00d413bc6d788a113
Vittore Zen <vittore@zen.pn.it>
2020-04-23 21:47:36 +0200

    First translation test

--- nav.php
+++ nav.php
@@ -5,40 +5,40 @@ $T = $CFG->wwwroot . '/';
 $adminmenu = isset($_COOKIE['adminmenu']) && $_COOKIE['adminmenu'] == "true";
 $set = new \Tsugi\UI\MenuSet();
 $set->setHome($CFG->servicename, $CFG->apphome);
-$set->addLeft('Get Started', $R.'install');
-$set->addLeft('Lessons', $R.'lessons');
+$set->addLeft('Iniziare', $R.'install');
+$set->addLeft('Lezioni', $R.'lessons');
 if ( isset($_SESSION['id']) ) {
-    $set->addLeft('Assignments', $R.'assignments');
+    $set->addLeft('Compiti', $R.'assignments');
     // $set->addLeft('Discuss', $R.'group');
     // If both are set we go to discuss.php
     // if ( isset($CFG->disqushost) ) $set->addLeft('Discuss', $T.'discuss');
     // else if ( isset($CFG->disquschannel) ) $set->addLeft('Discuss', $CFG->disquschannel);
 } else {
-    $set->addLeft('Materials', $R.'materials');
+    $set->addLeft('Materiali', $R.'materials');
 }
 
 if ( isset($_SESSION['id']) ) {
     $submenu = new \Tsugi\UI\Menu();
-    $submenu->addLink('Profile', $R.'profile');
+    $submenu->addLink('Profilo', $R.'profile');
     if ( isset($CFG->google_map_api_key) ) {
-        $submenu->addLink('Map', $R.'map');
+        $submenu->addLink('Mappa', $R.'map');
     }
 
-    $submenu->addLink('Badges', $R.'badges');
-    $submenu->addLink('Materials', $R.'materials');
+    $submenu->addLink('Badge', $R.'badges');
+    $submenu->addLink('Materiali', $R.'materials');
     if ( $CFG->providekeys ) {
-        $submenu->addLink('LMS Integration', $T . 'settings');
+        $submenu->addLink('Integrazioni con LMS', $T . 'settings');
     }
     if ( isset($CFG->google_classroom_secret) ) {
         $submenu->addLink('Google Classroom', $T.'gclass/login');
     }
-    $submenu->addLink('Free App Store', 'https://www.tsugicloud.org');
-    $submenu->addLink('Rate this course', 'https://www.class-central.com/mooc/7363/python-for-everybody');
+    $submenu->addLink('App Store gratuito', 'https://www.tsugicloud.org');
+    $submenu->addLink('Valuta questo corso', 'https://www.class-central.com/mooc/7363/python-for-everybody');
     $submenu->addLink('Privacy', $R.'privacy');
     if ( isset($_COOKIE['adminmenu']) && $_COOKIE['adminmenu'] == "true" ) {
-        $submenu->addLink('Administer', $T . 'admin/');
+        $submenu->addLink('Amministra', $T . 'admin/');
     }
-    $submenu->addLink('Logout', $R.'logout');
+    $submenu->addLink('Esci', $R.'logout');
 
     if ( isset($_SESSION['avatar']) ) {
         $set->addRight('<img src="'.$_SESSION['avatar'].'" style="height: 2em;"/>', $submenu);
@@ -47,15 +47,15 @@ if ( isset($_SESSION['id']) ) {
         $set->addRight(htmlentities($_SESSION['displayname']), $submenu);
     }
 } else {
-    $set->addRight('Login', $R.'login');
+    $set->addRight('Accedi', $R.'login');
 }
 
 $imenu = new \Tsugi\UI\Menu();
 
-$imenu->addLink('Instructor', 'http://www.dr-chuck.com');
+$imenu->addLink('Insegnante', 'http://www.dr-chuck.com');
 $imenu->addLink('Office Hours', 'http://www.dr-chuck.com/office/');
-$set->addRight('Book', $R . 'book');
-$set->addRight('Instructor', $imenu);
+$set->addRight('Libro', $R . 'book');
+$set->addRight('Insegnante', $imenu);
 
 // Set the topNav for the session
 $OUTPUT->topNavSession($set);
